7d72657f12e5f63230aa92c7fad77b3bd9c905c3
Zack Loveless <bhmath@gmail.com>
2014-10-28 19:58:14 -0700

    Move all EventArgs into a single file for simplicity's sake.

---
+++ Code/Atlantis.Net.Irc/IRfcCommand.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="IRfcCommand.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Atlantis.Net.Irc
+{
+	using System;
+
+	public interface IRfcCommand
+	{
+		/// <summary>
+		/// Returns the name of the command being executed or a generic identifier in the case of numerics handling.
+		/// </summary>
+		String Name { get; }
+
+		/// <summary>
+		/// Handles this command using the specified parameters.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="parameters"></param>
+		void Handle(String source, String[] parameters);
+	}
+
+	public abstract class RfcCommand : IRfcCommand
+	{
+		protected readonly IrcClient origin;
+
+		protected RfcCommand(IrcClient origin)
+		{
+			this.origin = origin;
+		}
+
+		#region Implementation of IRfcCommand
+
+		/// <summary>
+		/// Returns the name of the command being executed or a generic identifier in the case of numerics handling.
+		/// </summary>
+		public abstract string Name { get; }
+
+		/// <summary>
+		/// Handles this command using the specified parameters.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="parameters"></param>
+		public abstract void Handle(string source, string[] parameters);
+
+		#endregion
+	}
+}
---
+++ Code/Atlantis.Net.Irc/IrcClientEventArgs.cs
@@ -0,0 +1,193 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="IrcClientEventArgs.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Atlantis.Net.Irc
+{
+	using System;
+	using System.Text.RegularExpressions;
+
+	#region External type: JoinPartEventArgs
+
+	public class JoinPartEventArgs : EventArgs
+	{
+		public JoinPartEventArgs(string nick, string channel)
+		{
+			Channel = channel;
+			Nick = nick;
+		}
+
+		/// <summary>
+		/// Gets a <see cref="T:System.String" /> value representing the name of the user that joined or parted the specified channel.
+		/// </summary>
+		public string Nick { get; private set; }
+
+		/// <summary>
+		/// Gets a <see cref="T:System.String" /> value representing the target of the join or part event.
+		/// </summary>
+		public string Channel { get; private set; }
+	}
+
+	#endregion
+
+	#region External type: MessageReceivedEventArgs
+
+	public class MessageReceivedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:System.EventArgs" /> class.
+		/// </summary>
+		public MessageReceivedEventArgs(string source, string target, string message)
+		{
+			Source = source;
+			Message = message;
+			Target = target;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Source { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Target { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Message { get; private set; }
+	}
+
+	#endregion
+
+	#region External type: NickChangeEventArgs
+
+	public class NickChangeEventArgs : EventArgs
+	{
+		public NickChangeEventArgs(string oldNick, string newNick)
+		{
+			OldNick = oldNick;
+			NewNick = newNick;
+		}
+
+		public string OldNick { get; set; }
+
+		public string NewNick { get; set; }
+	}
+
+	#endregion
+
+	#region External type: ProtocolMessageEventArgs
+
+	public class ProtocolMessageEventArgs : EventArgs
+	{
+		public ProtocolMessageEventArgs(Match match, string message)
+		{
+			Message = message;
+			Match = match;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public Match Match { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Message { get; private set; }
+	}
+
+	#endregion
+
+	#region External type: QuitEventArgs
+
+	public class QuitEventArgs : EventArgs
+	{
+		public QuitEventArgs(string source, string message)
+		{
+			Nick = source;
+			Message = message;
+		}
+
+		public string Nick { get; private set; }
+
+		public string Message { get; private set; }
+	}
+
+	#endregion
+	
+	#region External type: RfcNumericReceivedEventArgs
+
+	public class RfcNumericReceivedEventArgs : EventArgs
+	{
+		public RfcNumericReceivedEventArgs(int numeric, string message)
+		{
+			Numeric = numeric;
+			Message = message;
+		}
+
+		/// <summary>
+		/// Gets a <see cref="T:System.Int32" /> value representing the raw numeric received from the IRC server.
+		/// </summary>
+		public int Numeric { get; private set; }
+
+		/// <summary>
+		/// Gets a <see cref="T:System.String" /> value representing the message after the raw numeric.
+		/// </summary>
+		public string Message { get; private set; }
+	}
+
+	#endregion
+
+	#region External type: RawMessageEventArgs
+
+	public class RawMessageEventArgs : EventArgs
+	{
+		public RawMessageEventArgs(string message)
+		{
+			Message = message;
+			Tokens = message.Split(' ');
+		}
+
+		/// <summary>
+		/// Gets a <see cref="T:System.String" /> value representing the message received from the IRC server.
+		/// </summary>
+		public string Message { get; private set; }
+		
+		public string[] Tokens { get; private set; }
+	}
+
+	#endregion
+
+	#region External type: TimeoutEventArgs
+
+	public class TimeoutEventArgs : EventArgs
+	{
+		public bool Handled { get; set; }
+	}
+
+	#endregion
+
+	#region External class: CancelableEventArgs
+
+	public class CancelableEventArgs : EventArgs
+	{
+		public bool IsCancelled { get; set; }
+	}
+
+	#endregion
+
+	#region External class: HandledEventArgs
+
+	public class HandledEventArgs : EventArgs
+	{
+		public bool IsHandled { get; set; }
+	}
+
+	#endregion
+}
