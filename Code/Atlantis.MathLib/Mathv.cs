d401665c954ab54b89a773a7ebee49a31eff3dbc
Zack Loveless <bhmath@gmail.com>
2015-09-05 12:51:35 -0700

    bye-bye vectors. [they're returning]

---
+++ Code/Atlantis.MathLib/Mathv.cs
@@ -0,0 +1,16 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="Mathv.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Atlantis
+{
+    /// <summary>
+    /// Contains methods relating to vector and linear algebra
+    /// </summary>
+    public static class Mathv
+    {
+
+    }
+}
