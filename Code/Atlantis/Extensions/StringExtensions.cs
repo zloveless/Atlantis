2270ae5631ae16f234d382cf6aa8f395d64d9e5c
Zack Loveless <bhmath@gmail.com>
2015-09-18 14:04:38 -0700

    Remove deprecated TimeSpan.Prettify method.

--- Code/Atlantis/Extensions/StringExtensions.cs
+++ Code/Atlantis/Extensions/StringExtensions.cs
@@ -32,27 +32,6 @@ namespace Atlantis.Extensions
 			return source != null ? source.Trim(trimChars) : String.Empty;
 		}
 
-        [Obsolete("Removing shortly.")]
-		public static String Prettify(this TimeSpan source)
-		{
-			if (source == null)
-			{
-				throw new ArgumentNullException("source", "The specified System.TimeSpan was null. :(");
-			}
-
-            return source.ToString();
-
-            // TODO: Internationalization this method.
-            /*StringBuilder builder = new StringBuilder();
-
-			if (source.Days > 0) builder.AppendFormat("{0} days", source.Days);
-			if (source.Hours > 0) builder.AppendFormat(", {0} hours", source.Hours);
-			if (source.Minutes > 0) builder.AppendFormat(", {0} minutes", source.Minutes);
-			if (source.Seconds > 0) builder.AppendFormat(", {0} seconds", source.Seconds);
-
-			return builder.ToString().Trim(',', ' ');*/
-		}
-
 		public static String JoinFormat<T>(this IEnumerable<T> source, String separator, String format)
 		{ // Credit: http://stackoverflow.com/a/13395017
 			if (source == null)
