ac75a3ecf3de0574fb9f1ee230ff40ce5957a1e4
Zack Loveless <bhmath@gmail.com>
2016-01-25 14:33:15 -0700

    Add two extension methods to silence R#.

---
+++ Code/Atlantis/Linq/EnumerableExtensions.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------------
+//  <copyright file="EnumerableExtensions.cs" company="MultiPlayerForums.com">
+//      Copyright (c) MultiPlayerForums.com All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+namespace Atlantis.Linq
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Determines whether a sequence contains any elements.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static bool None<TSource>(this IEnumerable<TSource> source)
+        {
+            return !source.Any();
+        }
+
+        /// <summary>
+        /// Determines whether any element of a sequence satisfies a condition.
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            return !source.Any(predicate);
+        }
+    }
+}
