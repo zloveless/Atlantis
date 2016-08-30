4fe42ffa5fb336df259b8b95b8910fe0739856e9
Zack Loveless <bhmath@gmail.com>
2016-08-30 00:29:44 -0700

    Remove old math lib stuff.

--- Code/Atlantis.MathLib/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Atlantis.MathLib")]
-[assembly: AssemblyDescription("Atlantis.MathLib is a collection of math-related functions and classes whilst in school.")]
-[assembly: AssemblyFileVersion("4.2")]
-[assembly: AssemblyInformationalVersion("4.2.0-alpha")]
-[assembly: CLSCompliant(true)]
