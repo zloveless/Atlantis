51f8bffab8643c5b3925e47c61000cb6e8583aab
Zack Loveless <bhmath@gmail.com>
2017-11-15 10:46:04 -0700

    Upgrade Atlantis and Atlantis.Net.Irc to new csproj format. Targeting .NET Standard 1.4 and .NET 4.5.2.

--- Code/Atlantis.Net.Irc/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle("Atlantis.Net.Irc")]
-[assembly: AssemblyDescription("Atlantis.Net.Irc is an implementation of the IRC RFC (not 100%) for my own purposes. It features a very fast IRC message parser, asynchronous message transmission, an event system, and a basic command system.")]
-[assembly: AssemblyFileVersion("4.1")]
-[assembly: AssemblyInformationalVersion("4.1.5")]
-[assembly: CLSCompliant( true )]
--- Code/Atlantis/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Reflection;
-
-[assembly: AssemblyTitle( "Atlantis" )]
-[assembly: AssemblyDescription( "Atlantis is a collection of code that is common amongst many pet projects." )]
-[assembly: AssemblyFileVersion( "4.1" )]
-[assembly: AssemblyInformationalVersion( "4.1.2" )]
-[assembly: CLSCompliant( true )]
