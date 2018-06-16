548e4eb7e41c1b7d74e630d089b45c1440f1563f
Zack Loveless <bhmath@gmail.com>
2018-06-16 09:37:29 -0700

    Re-add GlobalAssemblyInfo.cs for Unity project. Fix submodule mistakes on git for Windows lib.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------------
+//  <copyright file="GlobalAssemblyInfo.cs" company="Zack Loveless">
+//      Copyright (c) Zack Loveless.  All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("Zack Loveless")]
+[assembly: AssemblyProduct("Atlantis Framework")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2014 Zack Loveless. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("4.1.*")]
